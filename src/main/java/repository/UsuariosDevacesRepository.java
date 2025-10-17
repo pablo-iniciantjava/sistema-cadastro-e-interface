36a3504ad67774ef8bc38da3065500585ae1a104
Pablo Vinicius Moreira Santos <pablo.santos@keeggo.com>
2025-10-17 12:00:55 -0300

    Projeto completo: Alteração interface web SSH funcional com execução de scripts

--- src/main/java/repository/UsuariosDevacesRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.projetojava.usuarios.repository;
-
-import org.example.projetojava.usuarios.model.UsuariosDevaces;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuariosDevacesRepository extends JpaRepository<UsuariosDevaces, Integer> {}
