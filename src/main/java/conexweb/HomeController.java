cfb99d7a1656c1de385d45874dc9c65c327387ca
Gabriel Miller Silone <g234930@dac.unicamp.br>
2025-10-16 21:41:05 -0300

    fixes issues

--- src/main/java/conexweb/HomeController.java
+++ src/main/java/conexweb/HomeController.java
@@ -1,13 +1,13 @@
 package org.example.projetojava.conexweb;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "🚀 Olá, mundo! Spring Boot funcionando!";
+        return "redirect:/sshservice/web";
     }
 }
